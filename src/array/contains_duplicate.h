c196d9eab9dbcd5ad58070cc0b7ca5c6f3acbe2e
chenchuanyin <chenchuanyinuestc@gmail.com>
2018-01-24 12:51:09 +0800

    repair contian_duplicate compile error

--- src/array/contains_duplicate.h
+++ src/array/contains_duplicate.h
@@ -5,6 +5,7 @@
 // function should return true if any value appears at least twice in the array,
 // and it should return false if every element is distinct.
 
+#include <algorithm>
 #include <map>
 #include <vector>
 
